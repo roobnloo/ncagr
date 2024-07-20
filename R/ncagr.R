f7cf286951bdcc2658325ab31473410327746ef4
roobnloo <71776622+roobnloo@users.noreply.github.com>
2024-07-20 01:08:07 +0000

    implement two stage adaptive weights

--- R/ncagr.R
+++ R/ncagr.R
@@ -53,16 +53,17 @@ ncagr <- function(responses, covariates, gmixpath = seq(0, 1, by = 0.1),
   intx_sds <- apply(intx, 2, sd)
   intx_scale <- scale(intx)
 
-  nodewise <- function(node) {
-    # if (verbose)
-    #   print(paste("Starting initial run for node", node))
+  nodewise <- function(node, adaptive = FALSE) {
     y <- responses[, node] - mean(responses[, node])
     intx_scale_node <- intx_scale[, -(seq(0, q) * p + node)]
 
     wl1 <- rep(1, q + bveclength)
     wl2 <- rep(1, q)
     if (adaptive) {
-      fit_ridge <- glmnet::glmnet(cbind(cov_scale, intx_scale_node), y, alpha = 0, standardize = FALSE, intercept = FALSE)
+      fit_ridge <- glmnet::glmnet(
+        cbind(cov_scale, intx_scale_node), y,
+        alpha = 0, standardize = FALSE, intercept = FALSE
+      )
       co <- stats::coef(fit_ridge, s = fit_ridge$lambda[100], exact = TRUE)@x
       wl1 <- 1 / abs(co)
       wl1[wl1 > 2000] <- 2000
@@ -81,21 +82,21 @@ ncagr <- function(responses, covariates, gmixpath = seq(0, 1, by = 0.1),
       maxit = maxit, tol = tol
     )
     if (verbose) {
-      message("Finished initial run for node ", node)
+      message(paste(node, " "), appendLF = FALSE)
     }
     return(list(
-      lambdas = nodereg["lambdapath"][[1]],
-      beta = nodereg["beta"][[1]] / intx_sds[-(seq(0, q) * p + node)],
-      gamma = nodereg["gamma"][[1]] / cov_sds,
-      varhat = nodereg["varhat"][[1]],
-      resid = nodereg["resid"][[1]],
-      objval = nodereg["objval"][[1]],
+      lambdas = nodereg$lambdapath,
+      beta = nodereg$beta / intx_sds[-(seq(0, q) * p + node)],
+      gamma = nodereg$gamma / cov_sds,
+      varhat = nodereg$varhat,
+      resid = nodereg$resid,
+      objval = nodereg$objval,
       wl1 = wl1,
       wl2 = wl2
     ))
   }
 
-  message("Begin initial run...\n")
+  message("Begin initial run...")
 
   if (ncores > 1) {
     reg_result <- parallel::mclapply(seq_len(p), nodewise, mc.cores = ncores)
@@ -114,7 +115,7 @@ ncagr <- function(responses, covariates, gmixpath = seq(0, 1, by = 0.1),
     l2_weights[, node] <- reg_result[[node]]$wl2
   }
 
-  message("Finished initial run")
+  message("\nFinished initial run")
   message("Begin cross-validation...")
 
   cv_lambda_idx <- vector(length = p)
@@ -128,9 +129,7 @@ ncagr <- function(responses, covariates, gmixpath = seq(0, 1, by = 0.1),
       lambdas[, node], gmixpath, l1_weights[, node], l2_weights[, node],
       maxit, tol, nfolds
     )
-    if (verbose) {
-      message("Done cross-validating node ", node)
-    }
+    message(paste(node, " "), appendLF = FALSE)
     return(cv_result)
   }
 
@@ -140,10 +139,6 @@ ncagr <- function(responses, covariates, gmixpath = seq(0, 1, by = 0.1),
     cv_results <- lapply(seq_len(p), cv_node)
   }
 
-  # if (any(sapply(cv_results, inherits, what = "try-error"))) {
-  #   browser()
-  # }
-
   for (node in seq_len(p)) {
     cv_mse[node, , , ] <- cv_results[[node]]
     minind <- arrayInd(which.min(cv_results[[node]]), dim(cv_results[[node]]))
@@ -152,7 +147,47 @@ ncagr <- function(responses, covariates, gmixpath = seq(0, 1, by = 0.1),
     cv_gmix_idx[node] <- minind[3]
   }
 
-  message("Finished cross validating all nodes")
+  message("\nFinished cross validating all nodes")
+
+  varhat <- varhat[cbind(cv_lambda_idx, cv_sglmix_idx, cv_gmix_idx, seq(p))]
+
+  if (adaptive) {
+    message("Beginning adaptive run")
+    if (ncores > 1) {
+      reg_result <- parallel::mclapply(seq_len(p), nodewise, adaptive = TRUE, mc.cores = ncores)
+    } else {
+      reg_result <- lapply(seq_len(p), nodewise, adaptive = TRUE)
+    }
+    message("\nFinished initial adaptive run")
+
+    for (node in seq_len(p)) {
+      lambdas[, node] <- reg_result[[node]]$lambdas
+      beta[, , , , node] <- reg_result[[node]]$beta
+      gamma[, , , , node] <- reg_result[[node]]$gamma
+      resid[, , , , node] <- reg_result[[node]]$resid
+      objval[, , , node] <- reg_result[[node]]$objval
+      l1_weights[, node] <- reg_result[[node]]$wl1
+      l2_weights[, node] <- reg_result[[node]]$wl2
+    }
+
+    message("Begin cross-validation...")
+
+    if (ncores > 1) {
+      cv_results <- parallel::mclapply(seq_len(p), cv_node, mc.cores = ncores)
+    } else {
+      cv_results <- lapply(seq_len(p), cv_node)
+    }
+
+    message("\nFinished adaptive run")
+
+    for (node in seq_len(p)) {
+      cv_mse[node, , , ] <- cv_results[[node]]
+      minind <- arrayInd(which.min(cv_results[[node]]), dim(cv_results[[node]]))
+      cv_lambda_idx[node] <- minind[1]
+      cv_sglmix_idx[node] <- minind[2]
+      cv_gmix_idx[node] <- minind[3]
+    }
+  }
 
   ghat_select <- cbind(
     rep(seq(q), times = p),
@@ -162,8 +197,6 @@ ncagr <- function(responses, covariates, gmixpath = seq(0, 1, by = 0.1),
     rep(seq(p), each = q)
   )
   ghat_mx <- t(matrix(gamma[ghat_select], nrow = q, ncol = p))
-
-  varhat <- varhat[cbind(cv_lambda_idx, cv_sglmix_idx, cv_gmix_idx, seq(p))]
 
   bhat_select <- cbind(
     rep(seq(bveclength), times = p),
